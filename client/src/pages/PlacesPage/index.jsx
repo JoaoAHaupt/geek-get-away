4acfbd29af25bc053232bd068a37c624be27f564
JoaoAHaupt <joao.haupt@aluno.ifsp.edu.br>
2024-04-09 23:28:24 -0300

    cansado

--- client/src/pages/PlacesPage/index.jsx
+++ client/src/pages/PlacesPage/index.jsx
@@ -28,6 +28,7 @@ export const PlacesPage = () =>{
                             </div>
                             <input placeholder='Example: Bag End, Finn and Jake House '></input>
                         </div>
+                       
                         <div className='input-div'>
                             <div className='text-div'>
                                 <h2>Description</h2>
@@ -35,6 +36,7 @@ export const PlacesPage = () =>{
                             </div>
                             <textarea  placeholder="Example: It's a very good and comfy place... beside the dragons and the skeletons" cols="20" rows="5"></textarea>
                         </div>
+
                 
                         <div className='input-div'>
                             <div className='text-div'>
@@ -46,26 +48,46 @@ export const PlacesPage = () =>{
                                 <SubmitPhotoButton/> 
                             </div>
                             <div className='img-input'> 
-                                <button>+</button>
+                                <button>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5" />
+                                </svg>
+                                </button>
                             </div>
                         </div>
 
+                        
                         <div className='input-div-check'>
-                            <div className='text-div'>
-                                <h2>Timing</h2>
-                            </div>
-                            <div>
-                                <div className='input-time-div'>
-                                    <p>Check In</p>
-                                    <input type="time"></input> 
-                                </div>
-                                <div className='input-time-div'>
-                                    <p>Check Out</p>
-                                    <input type="time"></input> 
-                                </div>
-                            </div>
-                      
-                         </div>
+                             <div className='input-time-div'>
+                                 <p>Check In</p>
+                                 <input type="time"></input> 
+                             </div>
+                             <div className='input-time-div'>
+                                 <p>Check Out</p>
+                                 <input type="time"></input> 
+                             </div>
+                        </div>
+                        
+                        <div className='text-div'>
+                            <h2>Perks</h2>
+                            <h3><i>Some characteristics of the place and the location</i></h3>
+                        </div>
+                        <div style={{display:'grid'}}>
+                            
+                            <label>
+                                <input type="checkbox"/>
+                                <span>Magic</span>
+                            </label>
+                            <label >
+                                <input type="checkbox"/>
+                                <span>Monster pet Friendly</span>
+                            </label>
+                            <label >
+                                <input type="checkbox"/>
+                                <span>Private dragon parking spot</span>
+                            </label>
+                        </div>
+
                         <button></button>
                 </form>
             )}
